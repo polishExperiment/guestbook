b0e21b0e217ac43413e8b18b3a3b6fe21016cf22
Paweł Zwoliński <p.zwolin@gmail.com>
2022-06-20 20:27:54 +0200

    Add akismet spam checker

---
+++ config/secrets/dev/dev.encrypt.public.php
@@ -0,0 +1,3 @@
+<?php // dev.encrypt.public on Mon, 20 Jun 2022 20:02:00 +0200
+
+return "\xBE\xA9M\x15\xCAgV\x3E\xA4\xA2T\xA8\xE1b\x02\xA5\xE8.\xC8K\x14s\x0EIg~mc\xE67\xE3e";
--- src/Controller/ConferenceController.php
+++ src/Controller/ConferenceController.php
@@ -7,6 +7,7 @@ use App\Entity\Conference;
 use App\Form\CommentFormType;
 use App\Repository\CommentRepository;
 use App\Repository\ConferenceRepository;
+use App\SpamChecker;
 use Doctrine\ORM\EntityManagerInterface;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\File\Exception\FileException;
@@ -39,6 +40,7 @@ class ConferenceController extends AbstractController
         Request $request,
         Conference $conference,
         CommentRepository $commentRepository,
+        SpamChecker $spamChecker,
         string $photoDir): Response
     {
         $comment = new Comment();
@@ -57,6 +59,17 @@ class ConferenceController extends AbstractController
             }
 
             $this->em->persist($comment);
+            
+            $context = [
+                'user_ip' => $request->getClientIp(),
+                'user_agent' => $request->headers->get('user-agent'),
+                'referrer' => $request->headers->get('referer'),
+                'permalink' => $request->getUri(),
+            ];
+            if (2 === $spamChecker->getSpamScore($comment, $context)) {
+                throw new \RuntimeException('Blatant spam, go away!');
+            }
+
             $this->em->flush();
 
             return $this->redirectToRoute('conference' ,['slug' => $conference->getSlug()]);
